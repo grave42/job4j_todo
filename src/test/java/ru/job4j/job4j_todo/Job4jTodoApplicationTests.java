277aa5b014ea4e5b504d2e857a6a559a0bbee5f5
Филипп Кучеров <filipp.kucherov@gazprombank.ru>
2024-09-12 18:45:03 +0700

    first commit

---
+++ src/test/java/ru/job4j/job4j_todo/Job4jTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.job4j_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f7e49cb80cac3a2bd06d4e84cf5b62c381ff3fb1
Кучеров Филипп <filipp.kucherov@gazprombank.ru>
2024-11-05 12:16:39 +0700

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/TaskRepositoryImpl.java
+++ src/main/java/ru/job4j/todo/repository/TaskRepositoryImpl.java
@@ -8,168 +8,72 @@ import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class TaskRepositoryImpl implements TaskRepository {
 
-    private final SessionFactory sessionFactory;
+    private final CrudRepository crudRepository;
 
-    public TaskRepositoryImpl(final SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public TaskRepositoryImpl(final CrudRepository crudRepository) {
+        this.crudRepository = crudRepository;
     }
 
     @Override
     public Task save(final Task task) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.save(task);
-            transaction.commit();
-            return task;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new RuntimeException("Error saving task", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        crudRepository.run(session -> session.save(task));
+        return task;
     }
 
     @Override
     public boolean deleteById(final int id) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            Query query = session.createQuery("delete from Task where id = :id");
-            query.setParameter("id", id);
-            int result = query.executeUpdate();
-            transaction.commit();
-            return result > 0;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new RuntimeException("Error deleting task", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        String query = "delete from Task where id = :id";
+        Map<String, Object> params = Map.of("id", id);
+        crudRepository.run(query, params);
+        return true;
     }
 
     @Override
     public boolean update(final Task task) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            Query query = session.createQuery(
-                    "update Task set description = :description, title = :title, created = :created, done = :done where id = :id");
-            query.setParameter("description", task.getDescription());
-            query.setParameter("title", task.getTitle());
-            query.setParameter("created", task.getCreated());
-            query.setParameter("done", task.isDone());
-            query.setParameter("id", task.getId());
-            int result = query.executeUpdate();
-            transaction.commit();
-            return result > 0;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new RuntimeException("Error updating task", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        String query = "update Task set description = :description, title = :title, created = :created, done = :done where id = :id";
+        Map<String, Object> params = Map.of(
+                "description", task.getDescription(),
+                "title", task.getTitle(),
+                "created", task.getCreated(),
+                "done", task.isDone(),
+                "id", task.getId()
+        );
+        crudRepository.run(query, params);
+        return true;
     }
 
     @Override
     public Optional<Task> findById(final int id) {
-        Session session = null;
-        try {
-            session = sessionFactory.openSession();
-            Task task = session.find(Task.class, id);
-            return Optional.ofNullable(task);
-        } catch (Exception e) {
-            throw new RuntimeException("Error finding task by ID", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        String query = "from Task where id = :id";
+        Map<String, Object> params = Map.of("id", id);
+        return crudRepository.optional(query, Task.class, params);
     }
 
     @Override
     public Collection<Task> findByDone(final boolean done) {
-        Session session = null;
-        try {
-            session = sessionFactory.openSession();
-            Query<Task> query = session.createQuery("from Task where done = :done", Task.class);
-            query.setParameter("done", done);
-            return query.list();
-        } catch (Exception e) {
-            throw new RuntimeException("Error finding tasks by done status", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        String query = "from Task where done = :done";
+        Map<String, Object> params = Map.of("done", done);
+        return crudRepository.query(query, Task.class, params);
     }
 
     @Override
     public Collection<Task> findAll() {
-        Session session = null;
-        try {
-            session = sessionFactory.openSession();
-            Query<Task> query = session.createQuery("from Task", Task.class);
-            return query.list();
-        } catch (Exception e) {
-            throw new RuntimeException("Error finding all tasks", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        String query = "from Task";
+        return crudRepository.query(query, Task.class);
     }
 
     @Override
     public boolean updateDoneById(int id, boolean done) {
-        Session session = null;
-        Transaction transaction = null;
-        boolean isUpdated = false;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-
-            Query query = session.createQuery("update Task set done = :done where id = :id");
-            query.setParameter("done", done);
-            query.setParameter("id", id);
-
-            int result = query.executeUpdate();
-            transaction.commit();
-            isUpdated = result > 0;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return isUpdated;
+        String query = "update Task set done = :done where id = :id";
+        Map<String, Object> params = Map.of("done", done, "id", id);
+        crudRepository.run(query, params);
+        return true;
     }
-
 }
 
